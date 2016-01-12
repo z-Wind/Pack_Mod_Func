df4408e40ff3d38882d86e1fe115cc7e5142cfd1
z-Wind <zpsyhapcst@gmail.com>
2016-01-12 20:35:17 +0800

    init

---
+++ Package/Module1.py
@@ -0,0 +1,16 @@
+variable1 = 1
+variable2 = 'Module1'
+
+
+def Function1():
+    print('run Module1.py Function1')
+    
+def Function2():
+    print('run Module1.py function2')
+    
+def main():
+    print('run Module1.py')
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ Package/Module2.py
@@ -0,0 +1,16 @@
+variable1 = 2
+variable2 = 'Module2'
+
+
+def Function1():
+    print('run Module2.py Function1')
+    
+def Function2():
+    print('run Module2.py function2')
+    
+def main():
+    print('run Module2.py')
+    
+    
+if __name__ == '__main__':
+    main()
